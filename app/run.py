0662592239381c6251491886916b52fba02dc5b9
therealthingy <therealthingy95@gmail.com>
2021-11-15 13:09:49 +0100

    Adds dependency for swagger-ui

--- app/run.py
+++ app/run.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import connexion
+import os
+# import app
 
 
-# if __name__ == '__main__':
-#     app = connexion.App(__name__, specification_dir='/api/')
-#     app.add_api('rpi-pwm.yaml')
-#     app.run(port=8080)
+_DEFAULT_PORT = 8080
 
 
-
-
+if __name__ == '__main__':
+    app = connexion.App(__name__, specification_dir='api/')
+    app.add_api('specification.yaml', resolver_error=501)
+    app.run(port=int(os.getenv('API_SERVER_PORT') or _DEFAULT_PORT))
