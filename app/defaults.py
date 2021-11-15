97be0e3bab7334e31d6ef0ead84ddea958b6666c
therealthingy <therealthingy95@gmail.com>
2021-11-15 16:16:02 +0100

    Adds kill switch for API

--- app/defaults.py
+++ app/defaults.py
@@ -2,6 +2,8 @@
 Default config flags (may be overwritten via environment variable)
 '''
 
+
+DEFAULT_API_ENABLED = True
 DEFAULT_API_SERVER_PORT = 8080
 DEFAULT_API_AUTH_USER = "admin"
 DEFAULT_API_AUTH_PASS = "admin"
--- app/run.py
+++ app/run.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-from connexion import App as ConnexionApp, RestyResolver as ConnexionRestyResolver
 import os
+from defaults import DEFAULT_API_SERVER_PORT, DEFAULT_API_ENABLED
 # import app
-from defaults import DEFAULT_API_SERVER_PORT
 
+
+api_enabled = os.getenv('API_ENABLED') == 'True' or DEFAULT_API_ENABLED
 
 if __name__ == '__main__':
-    app = ConnexionApp(__name__, specification_dir='api/')
-    app.add_api('specification.yaml', resolver=ConnexionRestyResolver('api.handlers'), resolver_error=501)
-    app.run(port=int(os.getenv('API_SERVER_PORT') or DEFAULT_API_SERVER_PORT))
+    if api_enabled:
+        # TODO: Log starting API
+        from connexion import App as ConnexionApp, RestyResolver as ConnexionRestyResolver
+        webapi = ConnexionApp(__name__, specification_dir='api/')
+        webapi.add_api('specification.yaml', resolver=ConnexionRestyResolver('api.handlers'), resolver_error=501)
+        webapi.run(port=int(os.getenv('API_SERVER_PORT') or DEFAULT_API_SERVER_PORT))
