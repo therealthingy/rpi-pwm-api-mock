aa5c20b53cb327ce2e00c4acebe4afc3468d91da
therealthingy <therealthingy95@gmail.com>
2021-11-15 20:05:59 +0100

    Adds nop handlers for config & fanCurves

--- app/api/handlers/app/config.py
+++ app/api/handlers/app/config.py
@@ -2,13 +2,16 @@
 Operation handlers for endpoint `/app/config`
     Local dev server URL: http://127.0.0.1:8080/api/v1/app/config
 '''
-
-
 from connexion import NoContent
-
 
 def search():
     '''
     Handler for GET /app/config
     '''
-    return NoContent
+    return NoContent                        # TODO
+
+def put(body):
+    '''
+    Handler for GET /app/config
+    '''
+    return NoContent                        # TODO
---
+++ app/api/handlers/app/fanCurves.py
@@ -0,0 +1,23 @@
+'''
+Operation handlers for endpoint `/app/fanCurves`
+    Local dev server URL: http://127.0.0.1:8080/api/v1/app/fanCurves
+'''
+from connexion import NoContent
+
+def search():
+    '''
+    Handler for GET /app/fanCurves
+    '''
+    return NoContent                        # TODO
+
+def post(body):
+    '''
+    Handler for GET /app/fanCurves
+    '''
+    return NoContent                        # TODO
+
+def get(id):
+    '''
+    Handler for GET /app/fanCurves/{id}
+    '''
+    return NoContent                        # TODO
