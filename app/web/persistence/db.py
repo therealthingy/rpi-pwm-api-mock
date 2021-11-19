a9591c665dbba4fb8721772eb8f634aaa53aad20
therealthingy <therealthingy95@gmail.com>
2021-11-19 21:30:53 +0100

    Refactor db cols to snake_case

--- app/web/persistence/db.py
+++ app/web/persistence/db.py
@@ -25,7 +25,7 @@ class FanCurve(db.Model):
 
     id = db.Column(db.String(255), primary_key=True, default=lambda: str(uuid.uuid4()))
     name = db.Column(db.String(255), nullable=False)
-    fanCurveSeries = db.relationship("FanCurveSeriesPoint", backref="fan_curve",  lazy=False)  # One-To-Many unidirectional `FanCurve` -> `FanCurveSeriesPoint`
+    fan_curve_series = db.relationship("FanCurveSeriesPoint", backref="fan_curve", lazy=False)  # One-To-Many unidirectional `FanCurve` -> `FanCurveSeriesPoint`
 
     def __iter__(self):                 # Required when deserializing objects of type `Config` (otherwise TypeError b/c FanCurve not iterable)
         for attr in dir(self):
@@ -42,14 +42,14 @@ class FanCurveSeriesPoint(db.Model):
     __tablename__ = "fan_curve_point"
 
     id = db.Column(db.Integer, db.Sequence('fan_curve_point_seq', start=4, increment=1), primary_key=True)
-    fanDcInPerc = db.Column(db.Integer, nullable=False)  # TODO: min = 0, max = 100
-    tempInCels = db.Column(db.Integer, nullable=False)
-    fanCurve_id = db.Column(db.String(255), db.ForeignKey('fan_curve.id'))
+    fan_dc_in_perc = db.Column(db.Integer, nullable=False)  # TODO: min = 0, max = 100
+    temp_in_cels = db.Column(db.Integer, nullable=False)
+    fan_curve_id = db.Column(db.String(255), db.ForeignKey('fan_curve.id'))
 
 @event.listens_for(FanCurveSeriesPoint.__table__, 'after_create')
 def after_create_fancurvepoint_table(target, connection, **kw):
     connection.execute(
-        """INSERT INTO fan_curve_point  (id, fanDcInPerc, tempInCels, fanCurve_id)
+        """INSERT INTO fan_curve_point  (id, fan_dc_in_perc, temp_in_cels, fan_curve_id)
             VALUES
                 (0,  0, 45, "1C5A8579-AB76-4089-AF15-97FC1F4358AB"),
                 (1,  30, 46, "1C5A8579-AB76-4089-AF15-97FC1F4358AB"),
@@ -72,21 +72,21 @@ class Config(db.Model):
     __tablename__ = "config"
 
     id = db.Column(db.Integer, primary_key=True)
-    DCUpdateIntervalInSec = db.Column(db.Integer, nullable=False)
-    fanOn = db.Column(db.Boolean, nullable=False)
-    loggingEnabled = db.Column(db.Boolean, nullable=False)
-    loggingLevel = db.Column(db.Enum(LoggingLevel), nullable=False)
-    selectedFanCurve_id = db.Column(db.String(255),
+    dc_update_interval_in_sec = db.Column(db.Integer, nullable=False)
+    fan_on = db.Column(db.Boolean, nullable=False)
+    logging_enabled = db.Column(db.Boolean, nullable=False)
+    logging_level = db.Column(db.Enum(LoggingLevel), nullable=False)
+    selected_fancurve_id = db.Column(db.String(255),
                                     db.ForeignKey(FanCurve.id))  # One-To-One unidirectional Config -> FanCurve
-    selectedFanCurve = db.relationship(FanCurve, uselist=False, lazy=False)
-    pwmGpioPin = db.Column(db.Integer, nullable=False)
-    pwmInvertSignal = db.Column(db.Boolean, nullable=False)
-    pwmMaxDCInPerc = db.Column(db.Integer, nullable=False)
-    pwmMinDCInPerc = db.Column(db.Integer, nullable=False)
+    selected_fan_curve = db.relationship(FanCurve, uselist=False, lazy=False)
+    pwm_gpio_pin = db.Column(db.Integer, nullable=False)
+    pwm_invert_signal = db.Column(db.Boolean, nullable=False)
+    pwm_max_dc_in_perc = db.Column(db.Integer, nullable=False)
+    pwm_min_dc_in_perc = db.Column(db.Integer, nullable=False)
 
 @event.listens_for(Config.__table__, 'after_create')
 def after_create_config_table(target, connection, **kw):
     connection.execute(
-        """INSERT INTO config  (id, DCUpdateIntervalInSec, fanOn, loggingEnabled, loggingLevel, pwmGpioPin,
-                                pwmInvertSignal, pwmMaxDCInPerc, pwmMinDCInPerc, selectedFanCurve_id)
+        """INSERT INTO config  (id, dc_update_interval_in_sec, fan_on, logging_enabled, logging_level, pwm_gpio_pin,
+                                pwm_invert_signal, pwm_max_dc_in_perc, pwm_min_dc_in_perc, selected_fancurve_id)
            VALUES (0, 3, 0, 1, "WARN", 12, 0, 100, 0, "1C5A8579-AB76-4089-AF15-97FC1F4358AB");""")
