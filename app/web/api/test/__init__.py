b8cde820265be907df45f1dc891441423f9ef0bb
therealthingy <therealthingy95@gmail.com>
2021-11-22 10:58:47 +0100

    Minor refactorings

--- app/web/api/test/__init__.py
+++ app/web/api/test/__init__.py
@@ -3,14 +3,10 @@ import logging
 import connexion
 from flask_testing import TestCase
 
-from app.web.api.encoder import JSONEncoder
-
 
 class BaseTestCase(TestCase):
-
     def create_app(self):
         logging.getLogger('connexion.operation').setLevel('ERROR')
-        app = connexion.App(__name__, specification_dir='../openapi/')
-        app.app.json_encoder = JSONEncoder
+        app = connexion.App(__name__, specification_dir='../')
         app.add_api('openapi.yaml', pythonic_params=True)
         return app.app
--- app/web/api/test/test_api_controller.py
+++ app/web/api/test/test_api_controller.py
@@ -6,13 +6,6 @@ import unittest
 from flask import json
 from six import BytesIO
 
-from app.web.api.models.app_config import AppConfig  # noqa: E501
-from app.web.api.models.app_fan_curve import AppFanCurve  # noqa: E501
-from app.web.api.models.app_fan_curve_base import AppFanCurveBase  # noqa: E501
-from app.web.api.models.app_log_entry import AppLogEntry  # noqa: E501
-from app.web.api.models.app_temp_dc_history_entry import AppTempDcHistoryEntry  # noqa: E501
-from app.web.api.models.system_info import SystemInfo  # noqa: E501
-from app.web.api.models.system_process import SystemProcess  # noqa: E501
 from app.web.api.test import BaseTestCase
 
 
@@ -40,26 +33,26 @@ class TestDefaultController(BaseTestCase):
         Updates config flags
         """
         app_config = {
-  "fanOn" : true,
-  "loggingEnabled" : true,
-  "loggingLevel" : "WARN",
-  "dcUpdateIntervalInSec" : 3,
-  "selectedFanCurve" : {
-    "did" : "916CD0EB-A755-4663-8410-461431039F74",
-    "name" : "Quiet",
-    "fanCurveSeries" : [ {
-      "tempInCels" : 30,
-      "fanDcInPerc" : 40
-    }, {
-      "tempInCels" : 35,
-      "fanDcInPerc" : 50
-    } ]
-  },
-  "pwmGpioPin" : 12,
-  "pwmInvertSignal" : true,
-  "pwmMinDcInPerc" : 20,
-  "pwmMaxDcInPerc" : 95
-}
+            "fanOn" : True,
+            "loggingEnabled" : True,
+            "loggingLevel" : "WARN",
+            "dcUpdateIntervalInSec" : 3,
+            "selectedFanCurve" : {
+              "did" : "916CD0EB-A755-4663-8410-461431039F74",
+              "name" : "Quiet",
+              "fanCurveSeries" : [ {
+                "tempInCels" : 30,
+                "fanDcInPerc" : 40
+              }, {
+                "tempInCels" : 35,
+                "fanDcInPerc" : 50
+              } ]
+            },
+            "pwmGpioPin" : 12,
+            "pwmInvertSignal" : True,
+            "pwmMinDcInPerc" : 20,
+            "pwmMaxDcInPerc" : 95
+        }
         headers = { 
             'if_match': 'b5395865858689cfcb718a8fbf84e128771f6975',
             'Authorization': 'Basic Zm9vOmJhcg==',
@@ -109,15 +102,15 @@ class TestDefaultController(BaseTestCase):
         Updates requested fan curve whose id corresponds to specified \"did\"
         """
         app_fan_curve_base = {
-  "name" : "Quiet",
-  "fanCurveSeries" : [ {
-    "tempInCels" : 30,
-    "fanDcInPerc" : 40
-  }, {
-    "tempInCels" : 35,
-    "fanDcInPerc" : 50
-  } ]
-}
+            "name" : "Quiet",
+            "fanCurveSeries" : [ {
+              "tempInCels" : 30,
+              "fanDcInPerc" : 40
+            }, {
+              "tempInCels" : 35,
+              "fanDcInPerc" : 50
+            } ]
+        }
         headers = { 
             'if_match': 'b5395865858689cfcb718a8fbf84e128771f6975',
             'Authorization': 'Basic Zm9vOmJhcg==',
@@ -154,15 +147,15 @@ class TestDefaultController(BaseTestCase):
         Adds new fan curve
         """
         app_fan_curve_base = {
-  "name" : "Quiet",
-  "fanCurveSeries" : [ {
-    "tempInCels" : 30,
-    "fanDcInPerc" : 40
-  }, {
-    "tempInCels" : 35,
-    "fanDcInPerc" : 50
-  } ]
-}
+            "name" : "Quiet",
+            "fanCurveSeries" : [ {
+              "tempInCels" : 30,
+              "fanDcInPerc" : 40
+            }, {
+              "tempInCels" : 35,
+              "fanDcInPerc" : 50
+            } ]
+        }
         query_string = [('name', 'Quiet-')]
         headers = { 
             'Authorization': 'Basic Zm9vOmJhcg==',
